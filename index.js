2cef97a2d1c6f7bd1880afa40c5b5404c7b83a63
Alin Manole <alin.manole@yahoo.com>
2021-10-01 16:57:26 +0200

    4.57

---
+++ index.js
@@ -0,0 +1,27 @@
+// 01 - Alphabet
+
+// function sortLetters(text) {
+    
+//     return text.split('').sort().join('');
+// };
+
+// var text2 = sortLetters("konexio")
+// console.log(text2)
+
+
+function sortLetters2(str){
+    var arr = str.split('');
+    var sorted = arr.sort();
+    return sorted.join('');
+}
+var str2 = sortLetters2("konexio")
+console.log(str2)
+
+
+
+//   //02 - XOXO
+
+
+
+
+
